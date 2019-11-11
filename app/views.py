3b8f4565ce5cc5438b55501bda2f72bb6824e7cd
Ambiwlans <udp.castellani@gmail.com>
2019-11-11 12:47:33 -0500

    minor rework logging, resize footer font

--- app/views.py
+++ app/views.py
@@ -99,21 +99,21 @@ def sigmoid_cost_regularized(params, true_X, true_Y, last_t, last_a):
     #Penalize steepness while a is large
     reg += (np.log((t / 0.01)+3)) / (10 * (i**.75))
 
-    print("")
-    print("Cost on question #" +  str(i) +":")
-    print("t: " + str(t) + ", last_t: " + str(last_t))
-    print("a: " + str(a) + ", last_a: " + str(last_a))
-    print("----")
-
-    print("Jump size penalty")
-    print(np.log((t / last_t) + (last_t / t) - 1) / i)
-    print((abs(a - last_a) / last_a) / (4 * i))
-    print("Shallowness penalty")
-    print((np.log((0.01/t)+3)) / (((last_a/150)**3 + 1) * (i**.75)))
-    print("Steepness penalty")
-    print((np.log((t / 0.01)+3)) / (10 * (i**.75)))
-    print("Total:")
-    print(str(np.mean(((pred_Y - true_Y)**2)/weights)*(np.mean(weights))) + " + " + str(reg))
+#    print("")
+#    print("Cost on question #" +  str(i) +":")
+#    print("t: " + str(t) + ", last_t: " + str(last_t))
+#    print("a: " + str(a) + ", last_a: " + str(last_a))
+#    print("----")
+#
+#    print("Jump size penalty")
+#    print(np.log((t / last_t) + (last_t / t) - 1) / i)
+#    print((abs(a - last_a) / last_a) / (4 * i))
+#    print("Shallowness penalty")
+#    print((np.log((0.01/t)+3)) / (((last_a/150)**3 + 1) * (i**.75)))
+#    print("Steepness penalty")
+#    print((np.log((t / 0.01)+3)) / (10 * (i**.75)))
+#    print("Total:")
+#    print(str(np.mean(((pred_Y - true_Y)**2)/weights)*(np.mean(weights))) + " + " + str(reg))
     return np.mean(((pred_Y - true_Y)**2)/weights)*(np.mean(weights)) + reg
 
 ##########################################
@@ -235,15 +235,16 @@ def test():
                 searchkey = -searchkey + 1
             
             if x > current_app.config['MAX_X'] and x + searchkey < 1: 
-                print("Have asked every question!")
+                print("Test # " + str(session['testlogid']) + " asked every question!")
                 x = 1
                 #TODO - go to seperate page
                 break
                 
-        print ("Selected letter #" + str(x))
         newquestion = db.session.query(TestMaterial).filter(TestMaterial.my_rank == x).first()
 
         
+    print ("Asking test #" + str(session['testlogid']) + ", Question #: " + str(newquestion.id) + ", Rank: " + str(newquestion.my_rank) + " -- " + str(newquestion.kanji))
+    
     #Get some history to show
     oldquestions = history.order_by(QuestionLog.id.desc()).limit(100)
     
